26788d089413a53cebfa3b0af32884546a20b66b
Alex Kuntz <alex.kuntz@epitech.eu>
2020-12-03 00:40:41 +0100

    [ADD] front: routing w/ history // blank pages

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/Login'
+import Register from '../components/Register'
+import Home from '../components/Home'
+import Press from '../components/Press'
+import Article from '../components/Article'
+import NotFound from '../components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '*',
+      component: NotFound
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/press',
+      name: 'Press',
+      component: Press
+    },
+    {
+      path: '/article',
+      name: 'Article',
+      component: Article
     }
   ]
 })
