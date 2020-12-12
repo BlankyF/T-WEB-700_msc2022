d9d1ece7e5e6a11009283ae2f72cd0ee1d7e32c4
Alex Kuntz <alex.kuntz@epitech.eu>
2020-12-13 00:17:38 +0100

    [ADD] front: homePage: added graph, updated when clicking on crypt's number

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div v-if="status == 'test' || status == 'user'">
-      <!-- Insert Graph here -->
+      <div class="chart" v-if="dataCrypt">
+        <line-chart :chart-data="dataCrypt"></line-chart>
+      </div>
       <h2>List of all cryptocurrencies</h2>
       <div class="CryptList">
         <ul class="CryptLine">
@@ -16,7 +18,7 @@
       </div>
       <div class="CryptList" v-for="(crypto, index) in CryptArr" v-if="crypto.visible === true" v-bind:index="index" v-bind:key="crypto.id">
         <ul class="CryptLine">
-          <li class="CryptCol">{{ index+1 }}</li>
+          <li class="CryptCol"><button @click="fillData(crypto.openingPrice, crypto.currentPrice)">{{ index+1 }}</button></li>
           <li class="CryptCol">{{ crypto.name }}</li>
           <li class="CryptCol">{{ crypto.currentPrice }} $</li>
           <li class="CryptCol">{{ crypto.openingPrice }} $</li>
@@ -52,10 +54,13 @@
 </template>
 
 <script>
+import LineChart from './LineChart.js'
+
 /* TODO remove urlArr optArr, get status and CryptArr via api request */
 let urlArr = window.location.toString().split('?')
 let optArr
 let status
+let dataCrypt
 console.log('url', urlArr)
 if (urlArr[1]) {
   optArr = urlArr[1].split('&')
@@ -64,12 +69,44 @@ if (urlArr[1]) {
 }
 export default {
   name: 'Home',
+  components: {
+    LineChart
+  },
   data () {
     return {
       msg: 'Home',
       status: status,
-      CryptArr: [{name: 'Bitcoin', currentPrice: 17923, openingPrice: 18034, lowerPrice: 17276, highestPrice: 18107, visible: true}, {name: 'test', currentPrice: 17923, openingPrice: 18034, lowerPrice: 17276, highestPrice: 18107, visible: true}, {name: 'unseen', currentPrice: 17923, openingPrice: 18034, lowerPrice: 17276, highestPrice: 18107, visible: false}]
+      dataCrypt: dataCrypt,
+      CryptArr: [{name: 'Bitcoin', currentPrice: 18923, openingPrice: 18934, lowerPrice: 17276, highestPrice: 18107, visible: true}, {name: 'test', currentPrice: 15923, openingPrice: 12034, lowerPrice: 17276, highestPrice: 18107, visible: true}, {name: 'unseen', currentPrice: 17923, openingPrice: 18034, lowerPrice: 17276, highestPrice: 18107, visible: false}, {name: 'test2', currentPrice: 19923, openingPrice: 18034, lowerPrice: 17276, highestPrice: 18107, visible: true}]
     }
+  },
+  methods: {
+    fillData (openingPrice, currentPrice) {
+      this.dataCrypt = {
+        labels: ['Opening', 'Current'],
+        datasets: [
+          {
+            label: 'Opening to Current Price',
+            backgroundColor: '#f87979',
+            data: [openingPrice, currentPrice]
+          }
+        ]
+      }
+    }
+    /*
+    TODO: CryptData = {start: *start date of period*, end: *end date of period*, period: *selected period*, dataArray: [*data of price fluctuation*] }
+    fillData (CryptData) {
+      this.dataCrypt = {
+        labels: [CryptData.start, CryptData.end],
+        datasets: [
+          {
+            label: `${{ CryptData.period }} Price`,
+            backgroundColor: '#f87979',
+            data: CryptData.dataArray
+          }
+        ]
+      }
+    } */
   }
 }
 </script>
@@ -92,8 +129,15 @@ export default {
   width: 14%;
 }
 
+.chart {
+  max-width: 50%;
+  margin:  150px auto;
+}
+
 h1, h2 {
   font-weight: normal;
+  text-align: left;
+  margin-left: 15px;
 }
 ul {
   list-style-type: none;
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -6,6 +6,8 @@ import Home from '../components/Home'
 import Press from '../components/Press'
 import Article from '../components/Article'
 import NotFound from '../components/NotFound'
+import User from '../components/User'
+import UserCrypto from '../components/UserCrypto'
 
 Vue.use(Router)
 
@@ -40,6 +42,16 @@ export default new Router({
       path: '/article/:articleID',
       name: 'Article',
       component: Article
+    },
+    {
+      path: '/user',
+      name: 'User',
+      component: User
+    },
+    {
+      path: '/user/crypto',
+      name: 'UserCrypto',
+      component: UserCrypto
     }
   ]
 })
