33109f2b6e4fe122e49a8d686839ff84afc1d456
lucas DELOLME <lucas.delolme@orange.fr>
2020-12-13 17:14:02 +0100

    [FIX] -back 15s timer before database call

--- back/requires/app_v1-1.js
+++ back/requires/app_v1-1.js
@@ -29,7 +29,7 @@ app.use(session({
     cookie: { maxAge: 365 * 24 * 60 * 60 * 1000}
 }));
 
-refreshCryptoPrices();
+setTimeout(()=>refreshCryptoPrices(),1000*15);
 setInterval(() => refreshCryptoPrices(),1000*60*5);
 
 function refreshCryptoPrices(){
